f89ef40259079d183a6c53956b28f631b926049e
百溪 <2963637346@qq.com>
2025-03-26 00:23:21 +0800

    旧静态界面ing

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # flutterlearn2
 
 A new Flutter project.
+解决的问题 ：： 解决 pub 的静态包的引入问题 当只声明 assets/ 时，Flutter 默认只会打包子目录中明确匹配的文件类型（如 .png, .jpg, .svg 等）。 文件夹是无法识的 而且 assets 默认要在与 lib 平级的地方
+
+文件已经存在 无法创建 要先 clearn 在 run
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "222",
-      theme: ThemeData(primaryColor: Colors.blueAccent),
+      title: "Navi",
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 255, 255, 255),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Color(0xFF6000E6)),
+        ),
+        tabBarTheme: TabBarTheme(
+          labelColor: Color(0xFF6000E6),
+          unselectedLabelColor: Colors.grey,
+        ),
+      ),
       home: MyHome(),
     );
   }
@@ -28,59 +41,210 @@ class MyHome extends StatelessWidget {
           title: Text(
             "Navi",
             style: TextStyle(
-              color: Colors.blueAccent,
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+              color: Color(0xFFCDC7E8),
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
             ),
           ),
           centerTitle: true,
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(text: "主界面", icon: Icon(Icons.home)),
-            Tab(text: "消息", icon: Icon(Icons.email)),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                "lib/assets/icons/adduser.svg",
+                width: 20,
+                height: 20,
+              ),
+            ),
           ],
-        ),
-        body: TabBarView(children: [Text("12323"), Text("3333")]),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.all(0),
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text("ddd"),
-                accountEmail: Text("cccc"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                    'https://pica.zhimg.com/v2-3baea6dfc2d4acacd74b5e907278f94a_1440w.jpg',
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      'https://wy-static.wenxiaobai.com/aigc-online/delogo_df191513-1459-4102-96bb-42fe03b6b639.webp',
+          leading: Builder(
+            builder:
+                (context) => IconButton(
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  icon: Container(
+                    padding: EdgeInsetsDirectional.only(top: 10),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage(
+                        "lib/assets/images/userpic.jpg",
+                      ),
+                      radius: 15,
                     ),
                   ),
                 ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(1.0),
+            child: Container(
+              color: Color.fromARGB(255, 255, 255, 255),
+              height: 0.3,
+            ),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.grey[200]!)),
+          ),
+          child: SizedBox(
+            height: 50,
+            child: TabBar(
+              splashFactory: NoSplash.splashFactory,
+              overlayColor: MaterialStateProperty.all(Colors.transparent),
+              indicator: UnderlineTabIndicator(
+                borderSide: BorderSide.none,
+                insets: EdgeInsets.zero,
               ),
-              ListTile(
-                title: Text("123"),
-                trailing: Icon(Icons.add_a_photo_rounded),
-              ),
-              ListTile(
-                title: Text("123"),
-                trailing: Icon(Icons.add_a_photo_rounded),
-              ),
-              ListTile(
-                title: Text("123"),
-                trailing: Icon(Icons.add_a_photo_rounded),
-              ),
-              Divider(),
-              ListTile(
-                title: Text("123"),
-                trailing: Icon(Icons.add_a_photo_rounded),
-              ),
-            ],
+              indicatorColor: Colors.transparent,
+              labelColor: Color(0xFF6000E6),
+              unselectedLabelColor: Colors.grey,
+              labelStyle: TextStyle(fontSize: 12),
+              unselectedLabelStyle: TextStyle(fontSize: 12),
+              labelPadding: EdgeInsets.symmetric(vertical: 2),
+              tabs: [
+                Tab(icon: Icon(Icons.home, size: 20), text: "主界面"),
+                Tab(icon: Icon(Icons.email, size: 20), text: "消息"),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: [Center(child: Text("主界面内容")), Center(child: Text("消息内容"))],
+        ),
+        drawer: Theme(
+          data: Theme.of(
+            context,
+          ).copyWith(dividerTheme: DividerThemeData(color: Colors.transparent)),
+          child: Drawer(
+            backgroundColor: Colors.white,
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: [
+                      UserAccountsDrawerHeader(
+                        margin: EdgeInsets.zero,
+                        decoration: BoxDecoration(color: Colors.white),
+                        accountName: Text(
+                          "霸气小肥鹅",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Inter-Regular",
+                            color: Colors.black,
+                          ),
+                        ),
+                        accountEmail: Text(
+                          "@11111",
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                        currentAccountPicture: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: NetworkImage(
+                            'https://pica.zhimg.com/v2-3baea6dfc2d4acacd74b5e907278f94a_1440w.jpg',
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                        leading: SvgPicture.asset(
+                          'lib/assets/icons/Profile.svg',
+                          width: 24,
+                          height: 24,
+                        ),
+                        title: Text(
+                          "个人资料",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 117, 113, 206),
+                            fontFamily: "Inter-Regular",
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        leading: SvgPicture.asset(
+                          'lib/assets/icons/Vector1.svg',
+                          width: 24,
+                          height: 24,
+                        ),
+                        title: Text(
+                          "关注列表",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 117, 113, 206),
+                            fontFamily: "Inter-Regular",
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        leading: SvgPicture.asset(
+                          'lib/assets/icons/Vector.svg',
+                          width: 24,
+                          height: 24,
+                        ),
+                        title: Text(
+                          "编辑个人资料",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 117, 113, 206),
+                            fontFamily: "Inter-Regular",
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                      Divider(height: 1, color: Colors.grey[300]),
+                      ListTile(
+                        leading: SvgPicture.asset(
+                          'lib/assets/icons/information.svg',
+                          width: 24,
+                          height: 24,
+                        ),
+                        title: Text(
+                          "关于Navi",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 117, 113, 206),
+                            fontFamily: "Inter-Regular",
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                ),
+                // Bottom buttons row
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          // Settings action
+                        },
+                        child: Text(
+                          "退出",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 117, 113, 206),
+                            fontFamily: "Inter-Regular",
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Logout action
+                        },
+                        child: Text(
+                          "设置",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 117, 113, 206),
+                            fontFamily: "Inter-Regular",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
