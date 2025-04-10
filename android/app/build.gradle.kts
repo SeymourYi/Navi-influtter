0caae1ed076b0a7bae41b483d7cb123f45ae63d8
百溪 <2963637346@qq.com>
2025-04-10 18:00:40 +0800

    签名更换成功

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "com.Navi"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -19,6 +19,15 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("my-release-key.keystore")
+            storePassword = "123456"
+            keyAlias = "my-key-alias"
+            keyPassword = "123456"
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.Navi"
@@ -26,21 +35,19 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-  manifestPlaceholders["JPUSH_PKGNAME"] = "com.Navi"
+        versionCode = 3
+        versionName = "1.0.3"
+        manifestPlaceholders["JPUSH_PKGNAME"] = "com.Navi"
         manifestPlaceholders["JPUSH_APPKEY"] = "37bb58f488aa4f8dd7e43516" // NOTE: JPush 上注册的包名对应的 Appkey.
         manifestPlaceholders["JPUSH_CHANNEL"] = "Navi" //暂时填写默认值即可.
-  manifestPlaceholders["XIAOMI_APPID"] = "2882303761520401700" //暂时填写默认值即可.
-  manifestPlaceholders["XIAOMI_APPKEY"] = "5912040133700" //暂时填写默认值即可.
-
+        manifestPlaceholders["XIAOMI_APPID"] = "2882303761520401700" //暂时填写默认值即可.
+        manifestPlaceholders["XIAOMI_APPKEY"] = "5912040133700" //暂时填写默认值即可.
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // 使用我们定义的签名配置，而不是debug签名
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
